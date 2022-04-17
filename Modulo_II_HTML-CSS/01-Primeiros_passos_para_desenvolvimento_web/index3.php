56e79518ea3f9d4f3e47f44f6f6b50d60eaa625d
fariasraphael <farias.raphael@gmail.com>
2022-04-17 15:23:49 -0300

    adiciono exercicio de php do módulo II

---
+++ Modulo_II_HTML-CSS/01-Primeiros_passos_para_desenvolvimento_web/index3.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<title>Desafio Módulo II HTML/CSS - Primeiros passos para o desenvolvimento Web</title>
+		<link rel="stylesheet" href="//code.jquery.com/ui/1.13.1/themes/base/jquery-ui.css">
+		<link rel="stylesheet" href="/resources/demos/style.css">
+		<script src="https://code.jquery.com/jquery-3.6.0.js"></script>
+		<script src="https://code.jquery.com/ui/1.13.1/jquery-ui.js"></script>
+		<script>
+			$( function() {
+			$( "#accordion" ).accordion();
+			} );
+		</script>
+	</head>
+<body>
+ <?php
+ 
+ ?>
+<div id="accordion">
+  <h3>Painel Tipo Acordeon</h3>
+  <div>
+    <p>
+    RECEBA!! Obrigado Senhor!
+    </p>
+  </div>
+  <h3>Melhorias futuras</h3>
+  <div>
+    <p>
+		<ul>
+			<li>Reduzir o tamanho do painel cascata para um tamanho determinado;</li>
+			<li>Posicionar este painel em local especifico na pagina.</li>
+			<li>Deixar ele dinamico, adaptavel a diversos tamanhos de tela</li>
+			<li>Como iniciar ele fechado?</li>
+			<li>Como fazer fechar ele todo?</li>
+		</ul>
+    </p>
+  </div>
+  <h3>Mussum Ipsum parte 1</h3>
+  <div>
+    <p>
+    Mussum Ipsum, cacilds vidis litro abertis. Mauris nec dolor in eros commodo 
+	tempor. Aenean aliquam molestie leo, vitae iaculis nisl.Pra lá , depois 
+	divoltis porris, paradis.Viva Forevis aptent taciti sociosqu ad litora 
+	torquent.Todo mundo vê os porris que eu tomo, mas ninguém vê os tombis que 
+	eu levo!
+    </p>
+   </div>
+  <h3>Mussum Ipsum parte 2</h3>
+  <div>
+    <p>
+    Si u mundo tá muito paradis? Toma um mé que o mundo vai girarzis!Leite de 
+	capivaris, leite de mula manquis sem cabeça.Manduma pindureta quium dia 
+	nois paga.Diuretics paradis num copo é motivis de denguis.
+    </p>
+   </div>
+</div>
+ 
+ 
+</body>
+</html>
